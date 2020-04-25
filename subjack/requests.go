40661a98177a3cb6f83dea922bc622f708e5748b
sumgr0 <sumit.grover@gmail.com>
2020-04-25 17:35:53 +0530

    Update requests.go

--- subjack/requests.go
+++ subjack/requests.go
@@ -2,40 +2,34 @@ package subjack
 
 import (
 	"crypto/tls"
-	"net/http"
+	"github.com/valyala/fasthttp"
 	"time"
-
-	"github.com/go-resty/resty/v2"
 )
 
-func get(url string, ssl bool, followRedirects bool, userAgent string, timeout int) (body []byte) {
-	if ssl {
-		url = "https://" + url
-	} else {
-		url = "http://" + url
-	}
+func get(url string, ssl bool, timeout int) (body []byte) {
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI(site(url, ssl))
+	req.Header.Add("Connection", "close")
+	resp := fasthttp.AcquireResponse()
 
-	client := resty.New()
-	if followRedirects {
-		client.SetRedirectPolicy(resty.RedirectPolicyFunc(func(req *http.Request, via []*http.Request) error {
-			return nil
-		}))
-	} else {
-		client.SetRedirectPolicy(resty.RedirectPolicyFunc(func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		}))
-	}
+	client := &fasthttp.Client{TLSConfig: &tls.Config{InsecureSkipVerify: true}}
+	client.DoTimeout(req, resp, time.Duration(timeout)*time.Second)
 
-	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
-	client.SetHeader("User-Agent", userAgent)
-	client.SetHeader("Connection", "close")
-	client.SetTimeout(time.Duration(timeout) * time.Second)
-	client.SetCloseConnection(true)
-	client.SetDisableWarn(true)
-
-	resp, err := client.R().Get(url)
-	if err != nil {
-		return []byte{}
-	}
 	return resp.Body()
+}
+
+func https(url string, ssl bool, timeout int) (body []byte) {
+	newUrl := "https://" + url
+	body = get(newUrl, ssl, timeout)
+
+	return body
+}
+
+func site(url string, ssl bool) (site string) {
+	site = "http://" + url
+	if ssl {
+		site = "https://" + url
+	}
+
+	return site
 }
