10158f1a3607bc7d696e4f0e812c4a44076e4faa
sumgr0 <sumit.grover@gmail.com>
2023-05-20 23:55:53 +0530

    Update requests.go

--- subjack/requests.go
+++ subjack/requests.go
@@ -1,35 +1,57 @@
-package subjack
+package main
 
 import (
+	"bytes"
 	"crypto/tls"
+	"fmt"
 	"github.com/valyala/fasthttp"
 	"time"
 )
 
-func get(url string, ssl bool, timeout int) (body []byte) {
+var (
+	client = &fasthttp.Client{
+		TLSConfig: &tls.Config{InsecureSkipVerify: true},
+		MaxConnsPerHost: 1024, // Adjust the maximum number of connections per host as per your needs
+	}
+)
+
+func main() {
+	url := "example.com" // Replace with your desired URL
+	ssl := true          // Replace with your desired SSL value
+	timeout := 10        // Replace with your desired timeout in seconds
+
+	body := get(url, ssl, timeout)
+	fmt.Println(string(body))
+}
+
+func get(url string, ssl bool, timeout int) []byte {
 	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
 	req.SetRequestURI(site(url, ssl))
 	req.Header.Add("Connection", "close")
-	resp := fasthttp.AcquireResponse()
 
-	client := &fasthttp.Client{TLSConfig: &tls.Config{InsecureSkipVerify: true}}
-	client.DoTimeout(req, resp, time.Duration(timeout)*time.Second)
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	err := client.DoTimeout(req, resp, time.Duration(timeout) * time.Second)
+	if err != nil {
+		// Handle the error here
+	}
 
 	return resp.Body()
 }
 
-func https(url string, ssl bool, timeout int) (body []byte) {
-	newUrl := "https://" + url
-	body = get(newUrl, ssl, timeout)
+func site(url string, ssl bool) string {
+	var site bytes.Buffer
+	site.WriteString("http://")
+	site.WriteString(url)
 
-	return body
-}
-
-func site(url string, ssl bool) (site string) {
-	site = "http://" + url
 	if ssl {
-		site = "https://" + url
+		site.Reset()
+		site.WriteString("https://")
+		site.WriteString(url)
 	}
 
-	return site
+	return site.String()
 }
